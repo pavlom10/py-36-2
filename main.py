5456e4fb2058ba642ee4c47b94b2bc195981d8c8
pavlom <10door@gmail.com>
2021-03-15 17:38:02 +0300

    + Application folder

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import date
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(f"Today's date: {date.today()}")
+    calculate_salary()
+    get_employees()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
